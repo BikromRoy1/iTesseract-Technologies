08251aedda26856af87ca2635572c0ca370ae84e
BikromRoy1 <bikrom@peoplentech.com.bd>
2024-01-29 16:53:25 +0600

    Team page add magensment team

--- src/components/Pages/Team/mangTeam/MangTeam.jsx
+++ src/components/Pages/Team/mangTeam/MangTeam.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import './MangTeam.css';
 
 const MangTeam = () => {
@@ -38,18 +38,23 @@ const MangTeam = () => {
               <div className='grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-10 md:grid-cols-2 xl:grid-cols-4'>
                 {teamMembers?.map((teamMember) => (
                   <div key={teamMember.id} className='space-y-3'>
-                    <div className='cursor-pointer' title={teamMember?.name}>
-                      <img
-                        loading='lazy'
-                        className='rounded-lg hover:-translate-y-2 duration-200 ease-in-out  shadow-lg w-full object-cover'
-                        src={
-                          `https://itesseract.com.bd/master/` +
-                          teamMember?.image
-                        }
-                        alt={teamMember?.name}
-                      />
+                    <div
+                      className='w-full bg-white rounded-lg p-8 flex flex-col justify-center cursor-pointer items-center border-[1px] border-[#E5E7EB]'
+                      title={teamMember?.name}
+                    >
+                      <div className='mb-5'>
+                        <img
+                          loading='lazy'
+                          className='object-center object-cover rounded-full h-40 w-40'
+                          src={
+                            `https://itesseract.com.bd/master/` +
+                            teamMember?.image
+                          }
+                          alt={teamMember?.name}
+                        />
+                      </div>
                       <div className='text-center'>
-                        <h1 className='text-2xl font-bold mt-3 hover:text-[#1bb57b]'>
+                        <h1 className='text-[18px] font-bold hover:text-[#1bb57b]'>
                           {teamMember?.name}
                         </h1>
                         <p className='text-gray-400'>
