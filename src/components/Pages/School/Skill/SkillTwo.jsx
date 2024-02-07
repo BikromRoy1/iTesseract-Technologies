17df1b45a45af6e6d75602226241e5947620b387
BikromRoy1 <bikrom@peoplentech.com.bd>
2024-02-07 17:48:05 +0600

    skill page design new card

--- src/components/Pages/School/Skill/SkillTwo.jsx
+++ src/components/Pages/School/Skill/SkillTwo.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import onlineEducation from '../../../../Images/icons/online-education.png';
+import './Skill.css';
 
 const SkillTwo = () => {
   return (
@@ -15,6 +17,29 @@ const SkillTwo = () => {
             এবং কম্পিউটার বিজ্ঞানে দক্ষতা অর্জনের জন্য একটি সু-প্রতিষ্ঠিত
             পাঠ্যক্রম দ্বারা পরিচালিত হবেন।
           </p>
+        </div>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3'>
+          <div className='skill-items-card'>
+            <div className='skill-image mb-[13px]'>
+              <img
+                width='80'
+                height='80'
+                src={onlineEducation}
+                loading='lazy'
+                alt='icons'
+              />
+            </div>
+            <div className='skills-body'>
+              <h4 className='text-[20px] font-semibold mt-[16px] mb-[15px] tracking-[0.20000000298023224px] leading-[26px]'>
+                আর্টিফিশিয়াল ইন্টেলিজেন্স
+              </h4>
+              <p className='text-[16px] tracking-[0.20000000298023224px] leading-[24px] text-gray-500'>
+                শিশুদের জন্য কৃত্রিম বুদ্ধিমত্তার বেশ কয়েকটি সুবিধার মধ্যে একটি
+                হল সমস্যা সমাধানের ক্ষমতা উন্নত করা এবং মুখ শনাক্তকরণ প্রক্রিয়া
+                সম্পর্কে আরও ভাল বোঝা এবং জানা
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
