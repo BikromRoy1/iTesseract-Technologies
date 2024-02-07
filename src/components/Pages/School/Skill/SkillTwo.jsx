bf966117f37a644cb9db872d2c957d8e3bf1764b
BikromRoy1 <bikrom@peoplentech.com.bd>
2024-02-07 18:12:49 +0600

    skill all data maping in ui design

--- src/components/Pages/School/Skill/SkillTwo.jsx
+++ src/components/Pages/School/Skill/SkillTwo.jsx
@@ -11,6 +11,13 @@ const SkillTwo = () => {
       Description:
         'শিশুদের জন্য কৃত্রিম বুদ্ধিমত্তার বেশ কয়েকটি সুবিধার মধ্যে একটি হল সমস্যা সমাধানের ক্ষমতা উন্নত করা এবং মুখ শনাক্তকরণ প্রক্রিয়া সম্পর্কে আরও ভাল বোঝা এবং জানা',
     },
+    {
+      skill_id: '02',
+      skillsIcons: onlineEducation,
+      tittle: ' ডেটা সায়েন্স',
+      Description:
+        'ডেটা সায়েন্স হল বিশ্বের সবচেয়ে জনপ্রিয় এবং দ্রুতগতিতে বৃদ্ধি করছে বিশ্বব্যাপী প্রযুক্তি। পাইথন প্রোগ্রামিং ভাষা এবং iHero হার্ডওয়্যার সরঞ্জাম ব্যবহার করে একটি ডেটা সায়েন্স মাস্টার হয়ে উঠুন।',
+    },
   ];
 
   return (
@@ -28,26 +35,24 @@ const SkillTwo = () => {
             পাঠ্যক্রম দ্বারা পরিচালিত হবেন।
           </p>
         </div>
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3'>
-          {skills.map(() => (
-            <div className='skill-items-card'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+          {skills?.map((skill) => (
+            <div key={skill?.skill_id} className='skill-items-card'>
               <div className='skill-image mb-[13px]'>
                 <img
                   width='80'
                   height='80'
-                  src={onlineEducation}
+                  src={skill?.skillsIcons}
                   loading='lazy'
-                  alt='icons'
+                  alt={skill?.tittle}
                 />
               </div>
               <div className='skills-body'>
                 <h4 className='text-[20px] font-semibold mt-[16px] mb-[15px] tracking-[0.20000000298023224px] leading-[26px]'>
-                  আর্টিফিশিয়াল ইন্টেলিজেন্স
+                  {skill?.tittle}
                 </h4>
                 <p className='text-[16px] tracking-[0.20000000298023224px] leading-[24px] text-gray-500'>
-                  শিশুদের জন্য কৃত্রিম বুদ্ধিমত্তার বেশ কয়েকটি সুবিধার মধ্যে
-                  একটি হল সমস্যা সমাধানের ক্ষমতা উন্নত করা এবং মুখ শনাক্তকরণ
-                  প্রক্রিয়া সম্পর্কে আরও ভাল বোঝা এবং জানা
+                  {skill?.Description}
                 </p>
               </div>
             </div>
