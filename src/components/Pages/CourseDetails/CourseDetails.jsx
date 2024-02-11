61e2e2c7ece860b495dd019b18aa0ce728e0c176
BikromRoy1 <bikrom@peoplentech.com.bd>
2024-02-11 16:17:21 +0600

    Course videos file create page

--- src/components/Pages/CourseDetails/CourseDetails.jsx
+++ src/components/Pages/CourseDetails/CourseDetails.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import CourseVidoes from './CourseVidoes/CourseVidoes';
 import DetailsBanner from './DetailsBanner/DetailsBanner';
 
 const CourseDetails = () => {
   return (
     <div>
       <DetailsBanner></DetailsBanner>
+      <CourseVidoes></CourseVidoes>
     </div>
   );
 };
---
+++ src/components/Pages/CourseDetails/CourseVidoes/CourseVidoes.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CourseVidoes = () => {
+    return <div>
+      <h3>Hello</h3>
+  </div>;
+};
+
+export default CourseVidoes;
