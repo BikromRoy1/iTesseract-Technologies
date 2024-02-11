ccf17cc78762bb62f089ff9a7d03e35b47acbfc7
BikromRoy1 <bikrom@peoplentech.com.bd>
2024-02-11 16:41:09 +0600

    videosCurriculum componend page create

--- src/components/Pages/CourseDetails/CourseVidoes/CourseVidoes.jsx
+++ src/components/Pages/CourseDetails/CourseVidoes/CourseVidoes.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 
 const CourseVidoes = () => {
-    return (
-      <section id='videosCurriculum'>
-        <div className=' p-6 container mx-auto'></div>
-      </section>
-    );
+  return (
+    <section id='videosCurriculum'>
+      <div className=' p-6 container mx-auto'>
+        <div className='flex flex-col gap-4 md:flex-row md:gap-12 md:pt-10'>
+          <div className='md:max-w-[calc(100%_-_448px)] mx-md:order-2'>
+            <h5>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Voluptatibus fuga omnis magnam quos deleniti dolore esse modi
+              numquam non exercitationem!
+            </h5>
+          </div>
+          <div className=' w-full md:mb-10 md:max-w-[400px] mx-md:order-1'>
+            <h2>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias ex
+              enim placeat recusandae error porro id nobis laudantium, eius
+              eligendi ipsum tempora modi odio facere illo quisquam ratione
+              accusamus? Consectetur, voluptates? Quasi molestias nihil sequi
+              possimus velit, eaque omnis earum vero, excepturi aspernatur
+              dolore perspiciatis? Perferendis ratione iste aliquid nobis vel
+              qui ipsam provident. Atque soluta excepturi nostrum minus
+              perferendis deleniti amet hic!
+            </h2>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default CourseVidoes;
---
+++ src/components/Pages/CourseDetails/Videos/Videos.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Videos.css';
+
+const Videos = () => {
+  return (
+    <div>
+      <h2>Videos page </h2>
+    </div>
+  );
+};
+
+export default Videos;
