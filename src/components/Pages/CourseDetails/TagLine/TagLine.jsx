6e26dd811b83fbb5d1aa84661c8a13ffd8e4d1ed
BikromRoy1 <bikrom@peoplentech.com.bd>
2024-02-12 12:38:08 +0600

    Tag lines components create

---
+++ src/components/Pages/CourseDetails/TagLine/TagLine.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TagLine.css';
+
+const TagLine = () => {
+  return (
+    <div>
+      <h3>This is Tag Lines</h3>
+    </div>
+  );
+};
+
+export default TagLine;
