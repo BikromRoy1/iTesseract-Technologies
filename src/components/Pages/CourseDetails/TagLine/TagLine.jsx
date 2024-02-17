b204924c1d3d354fb06c068f90f66d2699e8c542
BikromRoy1 <bikrom@peoplentech.com.bd>
2024-02-17 10:44:59 +0600

    course skills add

--- src/components/Pages/CourseDetails/TagLine/TagLine.jsx
+++ src/components/Pages/CourseDetails/TagLine/TagLine.jsx
@@ -4,7 +4,15 @@ import './TagLine.css';
 const TagLine = () => {
   return (
     <div className='TagLine-card'>
-      <h3>আপনি যে দক্ষতাগুলি পাবেন</h3>
+      <h3>দক্ষতাগুলি পাবেন</h3>
+
+      <div class='TagLine'>
+        <h5 class='tag-color01'>কোডিং</h5>
+        <h5 class='bg-[#d7f5fc] text-[#03c3ec]'>লজিক</h5>
+        <h5 class='text-[#ffab00] bg-[#fff2d6]'>গেম ডেভেলপমেন্ট</h5>
+        <h5 class='bg-[#ffeef3] text-[#f8285a]'>স্ক্র্যাচ</h5>
+        <h5 class='text-[#1b84ff] bg-[#1b84ff1a]'>প্রোগ্রামিং বেসিক</h5>
+      </div>
     </div>
   );
 };
