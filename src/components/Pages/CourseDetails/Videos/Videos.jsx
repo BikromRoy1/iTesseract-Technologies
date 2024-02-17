64f9097f0de741d1f5918042bb871877c186bdbd
BikromRoy1 <bikrom@peoplentech.com.bd>
2024-02-17 11:08:23 +0600

    videos-timing add

--- src/components/Pages/CourseDetails/Videos/Videos.jsx
+++ src/components/Pages/CourseDetails/Videos/Videos.jsx
@@ -7,6 +7,20 @@ const Videos = () => {
       <div className='videos-title'>
         <h3 className='w-full'>কোর্সের পাঠ্যক্রম</h3>
       </div>
+      <div className='videos-timing'>
+        <ul className='flex pb-3'>
+          <li className='mr-[30px] text-[#7b7b8a] text-[17px] font-medium'>
+            {' '}
+            বিগিনার লেভেল
+          </li>
+          <li className='mr-[30px] text-[#7b7b8a] text-[17px] font-medium'>
+            30 ভিডিও
+          </li>
+          <li className='mr-[30px] text-[#7b7b8a] text-[17px] font-medium'>
+            মোট: 5 ঘন্টা 56 মিনিট 24 সেকেন্ড
+          </li>
+        </ul>
+      </div>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit,
         dolorum. Lorem ipsum dolor sit amet consectetur adipisicing elit.
