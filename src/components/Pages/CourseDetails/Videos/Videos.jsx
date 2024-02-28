40acdb995d99d4257c415109c3520f4ba03c6464
BikromRoy1 <bikrom@peoplentech.com.bd>
2024-02-28 14:21:07 +0600

    Vidoes section done

--- src/components/Pages/CourseDetails/Videos/Videos.jsx
+++ src/components/Pages/CourseDetails/Videos/Videos.jsx
@@ -11,10 +11,6 @@ const Videos = () => {
       <div className='videos-timing'>
         <ul className='flex pb-3 flex-wrap items-center'>
           <li className='mr-[30px] text-[#7b7b8a] text-[17px] font-medium'>
-            {' '}
-            বিগিনার লেভেল
-          </li>
-          <li className='mr-[30px] text-[#7b7b8a] text-[17px] font-medium'>
             30 ভিডিও
           </li>
           <li className='mr-[30px] text-[#7b7b8a] text-[17px] font-medium'>
@@ -23,12 +19,7 @@ const Videos = () => {
         </ul>
       </div>
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
-        voluptates illum quos dicta Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Minus, autem. Tempore fugit natus placeat illo, beatae
-        corporis ullam recusandae numquam.
-      </p>
+      
 
       <div className='videos-module'>
         <details
@@ -192,9 +183,8 @@ const Videos = () => {
         <details className='group rounded-lg   [&_summary::-webkit-details-marker]:hidden mb-[1rem]'>
           <summary className='flex items-center justify-between cursor-pointer bg-[#F5F7F9] py-[1rem] px-[1.25rem] rounded-[6px]'>
             <h2 className='font-bold text-[#124265] videos-class-title text-[14px] sm:text-[14px] md:text-[16px]'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit,
-              numquam.
-              <span className='font-medium text-[#7b7b8a]'> (10 Lectures)</span>
+              ক্লাস 01: স্ক্র্যাচ প্রোগ্রামিং এর বেসিক
+              <span className='font-medium text-[#7b7b8a]'> (06 Lectures)</span>
             </h2>
 
             <span className='relative ml-1.5 h-5 w-5 flex-shrink-0'>
@@ -230,10 +220,122 @@ const Videos = () => {
             </span>
           </summary>
 
-          <p className='mt-4 leading-relaxed text-gray-500 text-justify'>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Laudantium, velit facere consequuntur suscipit nesciunt aut officia!
-          </p>
+          <div className='mt-[1rem] pl-[1rem] pt-[0.2rem] pr-[0.5rem] pb-[0.5rem]'>
+            <div className='flex justify-between items-center flex-wrap'>
+              <div className='relative flex items-center videos-name-main flex-wrap'>
+                <a href='#' className='btn-danger-soft mb-0'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='23'
+                    height='23'
+                    fill='currentColor'
+                    className='bi bi-play-fill play-icons mr-0 pl-[3px]'
+                    viewBox='0 0 16 16'
+                  >
+                    <path d='m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393' />
+                  </svg>
+                </a>
+                <span className='inline-block truncate ml-2 mb-0 text-base font-normal w-100 sm:w-200 md:w-400 cursor-pointer'>
+                  Game development using blocks{' '}
+                  <span className='badge bg-[#FD7E14] border-none text-[14px] font-medium whitespace-nowrap text-white  ml-1 capitalize'>
+                    <FaLock className='w-[13px] h-[13px] mr-[3px]' />
+                    premium
+                  </span>
+                </span>
+              </div>
+              <p className='mb-0 text-[#7b7b8a] text-[16px] font-semibold'>
+                2m 10s
+              </p>
+            </div>
+
+            <hr className='hr-style my-[1rem] text-[#9a9ea4]' />
+
+            <div className='flex justify-between items-center'>
+              <div className='relative flex items-center videos-name-main'>
+                <a href='#' className='btn-danger-soft mb-0 flex-wrap'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='23'
+                    height='23'
+                    fill='currentColor'
+                    className='bi bi-play-fill play-icons mr-0 pl-[3px]'
+                    viewBox='0 0 16 16'
+                  >
+                    <path d='m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393' />
+                  </svg>
+                </a>
+                <span className='videos-name inline-block truncate ml-2 mb-0 text-base font-normal w-[100px] sm:w-[200px] md:w-[400px] cursor-pointer'>
+                  {' '}
+                  Scratch Components{' '}
+                  <span className='badge bg-[#FD7E14] border-none text-[14px] font-medium whitespace-nowrap text-white  ml-1 capitalize'>
+                    <FaLock className='w-[13px] h-[13px] mr-[3px]' />
+                    premium
+                  </span>
+                </span>
+              </div>
+              <p className='mb-0 text-[#7b7b8a] text-[16px] font-semibold'>
+                15m 10s
+              </p>
+            </div>
+
+            <hr className='hr-style my-[1rem] text-[#9a9ea4]' />
+
+            <div className='flex justify-between items-center  flex-wrap'>
+              <div className='relative flex items-center videos-name-main flex-wrap'>
+                <a href='#' class='btn-danger-soft mb-0'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='23'
+                    height='23'
+                    fill='currentColor'
+                    class='bi bi-play-fill play-icons mr-0 pl-[3px]'
+                    viewBox='0 0 16 16'
+                  >
+                    <path d='m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393' />
+                  </svg>
+                </a>
+                <span className='inline-block truncate ml-2 mb-0 text-base font-normal w-[100px] sm:w-[200px] md:w-[400px] cursor-pointer'>
+                  Knowing Your Topic and Specification{' '}
+                  <span className='badge bg-[#FD7E14] border-none text-[14px] font-medium whitespace-nowrap text-white  ml-1 capitalize'>
+                    <FaLock className='w-[13px] h-[13px] mr-[3px]' />
+                    premium
+                  </span>
+                </span>
+              </div>
+              <p className='mb-0 text-[#7b7b8a] text-[16px] font-semibold'>
+                18m 10s
+              </p>
+            </div>
+
+            <hr className='hr-style my-[1rem] text-[#9a9ea4]' />
+
+            <div className='flex justify-between items-center '>
+              <div className='relative flex items-center videos-name-main'>
+                <a href='#' class='btn-danger-soft mb-0'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='23'
+                    height='23'
+                    fill='currentColor'
+                    class='bi bi-play-fill play-icons mr-0 pl-[3px]'
+                    viewBox='0 0 16 16'
+                  >
+                    <path d='m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393' />
+                  </svg>
+                </a>
+                <span className='inline-block truncate ml-2 mb-0 text-base font-normal w-[100px] sm:w-[200px] md:w-[400px] cursor-pointer'>
+                  Introduction to Box Modelling{' '}
+                  <span className='badge bg-[#FD7E14] border-none text-[14px] font-medium whitespace-nowrap text-white  ml-1 capitalize'>
+                    <FaLock className='w-[13px] h-[13px] mr-[3px]' />
+                    premium
+                  </span>
+                </span>
+              </div>
+              <p className='mb-0 text-[#7b7b8a] text-[16px] font-semibold'>
+                25m 10s
+              </p>
+            </div>
+          </div>
         </details>
       </div>
     </div>
