89fc3a9410b388c348c71cef1f935c30943e6a07
BikromRoy1 <bikrom@peoplentech.com.bd>
2024-03-02 18:51:46 +0600

    real time vidoes add

--- src/components/Pages/CourseDetails/videosPlaying/VideosPlaying.jsx
+++ src/components/Pages/CourseDetails/videosPlaying/VideosPlaying.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { FaLock } from 'react-icons/fa';
 import animal from '../../../../Images/vidoes/animal.mp4';
 import bird from '../../../../Images/vidoes/bird.mp4';
@@ -40,10 +40,28 @@ const VideosPlaying = () => {
   ];
   const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
   const [activeVideoTitle, setActiveVideoTitle] = useState(videoData[0].title);
+  const [videoDuration, setVideoDuration] = useState('0:00'); // Initial duration
+  const videoRef = useRef(null);
 
   const changeVideoSource = (newIndex) => {
     setCurrentVideoIndex(newIndex);
     setActiveVideoTitle(videoData[newIndex].title);
+    setVideoDuration('0:00');
+  };
+
+  const handleLoadedMetadata = () => {
+    // Update the video duration when metadata is loaded
+    if (videoRef.current) {
+      const durationInSeconds = videoRef.current.duration;
+      const formattedDuration = formatVideoDuration(durationInSeconds);
+      setVideoDuration(formattedDuration);
+    }
+  };
+
+  const formatVideoDuration = (durationInSeconds) => {
+    const minutes = Math.floor(durationInSeconds / 60);
+    const seconds = Math.floor(durationInSeconds % 60);
+    return `${minutes}m ${seconds}s`;
   };
 
   return (
@@ -55,16 +73,18 @@ const VideosPlaying = () => {
               মোবাইল অ্যাপ্লিকেশন ও ওয়েবসাইট ডেভেলপমেন্ট
             </h4>
             <video
+              ref={videoRef}
               controls
               autoPlay
               width='100%'
               height='400px'
               src={videoData[currentVideoIndex].source}
               typeof='mp4'
+              onLoadedMetadata={handleLoadedMetadata}
             ></video>
 
             <h3 className='font-semibold  text-[16px] md:text-[22px] leading-[28px] pt-[20px]'>
-              Video: {activeVideoTitle}
+              Video: {activeVideoTitle} - Duration: {videoDuration}
             </h3>
           </div>
           <div>
@@ -161,7 +181,7 @@ const VideosPlaying = () => {
                             </span>
                           </div>
                           <p className='mb-0 text-[#7b7b8a] text-[16px] font-semibold flex items-center gap-2'>
-                            {data.duration}
+                            {videoDuration}
                           </p>
                         </div>
                         {index !== videoData.length - 1 && (
@@ -169,6 +189,166 @@ const VideosPlaying = () => {
                         )}
                       </div>
                     ))}
+                  </div>
+                </details>
+                <details className='group rounded-lg   [&_summary::-webkit-details-marker]:hidden mb-[1rem] bg-[#F5F7F9]'>
+                  <summary className='flex items-center justify-between cursor-pointer bg-[#F5F7F9] py-[1rem] px-[1.25rem] rounded-[6px]'>
+                    <h2 className='font-bold text-[#124265] videos-class-title text-[14px] sm:text-[14px] md:text-[16px]'>
+                      ক্লাস 01: স্ক্র্যাচ প্রোগ্রামিং এর বেসিক
+                      <span className='font-medium text-[#7b7b8a]'>
+                        {' '}
+                        (06 Lectures)
+                      </span>
+                    </h2>
+
+                    <span className='relative ml-1.5 h-5 w-5 flex-shrink-0'>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        className='absolute inset-0 w-5 h-5 opacity-100 group-open:opacity-0'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        stroke='currentColor'
+                        strokeWidth='2'
+                      >
+                        <path
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
+                          d='M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z'
+                        />
+                      </svg>
+
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        className='absolute inset-0 w-5 h-5 opacity-0 group-open:opacity-100'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        stroke='currentColor'
+                        strokeWidth='2'
+                      >
+                        <path
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
+                          d='M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z'
+                        />
+                      </svg>
+                    </span>
+                  </summary>
+
+                  <div className='mt-[1rem] pl-[1rem] pt-[0.2rem] pr-[0.5rem] pb-[0.5rem]'>
+                    <div className='flex justify-between items-center flex-wrap'>
+                      <div className='relative flex items-center videos-name-main flex-wrap'>
+                        <a href='#' className='btn-danger-soft mb-0'>
+                          <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            width='23'
+                            height='23'
+                            fill='currentColor'
+                            className='bi bi-play-fill play-icons mr-0 pl-[3px]'
+                            viewBox='0 0 16 16'
+                          >
+                            <path d='m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393' />
+                          </svg>
+                        </a>
+                        <span className='inline-block truncate ml-2 mb-0 text-base font-normal w-[100px] sm:w-[200px] md:w-[400px] cursor-pointer'>
+                          Game development using blocks{' '}
+                          <span className='badge bg-[#FD7E14] border-none text-[14px] font-medium whitespace-nowrap text-white  ml-1 capitalize'>
+                            <FaLock className='w-[13px] h-[13px] mr-[3px]' />
+                            premium
+                          </span>
+                        </span>
+                      </div>
+                      <p className='mb-0 text-[#7b7b8a] text-[16px] font-semibold'>
+                        2m 10s
+                      </p>
+                    </div>
+
+                    <hr className='hr-style my-[1rem] text-[#9a9ea4]' />
+
+                    <div className='flex justify-between items-center'>
+                      <div className='relative flex items-center videos-name-main'>
+                        <a href='#' className='btn-danger-soft mb-0 flex-wrap'>
+                          <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            width='23'
+                            height='23'
+                            fill='currentColor'
+                            className='bi bi-play-fill play-icons mr-0 pl-[3px]'
+                            viewBox='0 0 16 16'
+                          >
+                            <path d='m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393' />
+                          </svg>
+                        </a>
+                        <span className='videos-name inline-block truncate ml-2 mb-0 text-base font-normal w-[100px] sm:w-[200px] md:w-[400px] cursor-pointer'>
+                          {' '}
+                          Scratch Components{' '}
+                          <span className='badge bg-[#FD7E14] border-none text-[14px] font-medium whitespace-nowrap text-white  ml-1 capitalize'>
+                            <FaLock className='w-[13px] h-[13px] mr-[3px]' />
+                            premium
+                          </span>
+                        </span>
+                      </div>
+                      <p className='mb-0 text-[#7b7b8a] text-[16px] font-semibold'>
+                        15m 10s
+                      </p>
+                    </div>
+
+                    <hr className='hr-style my-[1rem] text-[#9a9ea4]' />
+
+                    <div className='flex justify-between items-center  flex-wrap'>
+                      <div className='relative flex items-center videos-name-main flex-wrap'>
+                        <a href='#' class='btn-danger-soft mb-0'>
+                          <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            width='23'
+                            height='23'
+                            fill='currentColor'
+                            class='bi bi-play-fill play-icons mr-0 pl-[3px]'
+                            viewBox='0 0 16 16'
+                          >
+                            <path d='m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393' />
+                          </svg>
+                        </a>
+                        <span className='inline-block truncate ml-2 mb-0 text-base font-normal w-[100px] sm:w-[200px] md:w-[400px] cursor-pointer'>
+                          Knowing Your Topic and Specification{' '}
+                          <span className='badge bg-[#FD7E14] border-none text-[14px] font-medium whitespace-nowrap text-white  ml-1 capitalize'>
+                            <FaLock className='w-[13px] h-[13px] mr-[3px]' />
+                            premium
+                          </span>
+                        </span>
+                      </div>
+                      <p className='mb-0 text-[#7b7b8a] text-[16px] font-semibold'>
+                        18m 10s
+                      </p>
+                    </div>
+
+                    <hr className='hr-style my-[1rem] text-[#9a9ea4]' />
+
+                    <div className='flex justify-between items-center '>
+                      <div className='relative flex items-center videos-name-main'>
+                        <a href='#' class='btn-danger-soft mb-0'>
+                          <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            width='23'
+                            height='23'
+                            fill='currentColor'
+                            class='bi bi-play-fill play-icons mr-0 pl-[3px]'
+                            viewBox='0 0 16 16'
+                          >
+                            <path d='m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393' />
+                          </svg>
+                        </a>
+                        <span className='inline-block truncate ml-2 mb-0 text-base font-normal w-[100px] sm:w-[200px] md:w-[400px] cursor-pointer'>
+                          Introduction to Box Modelling{' '}
+                          <span className='badge bg-[#FD7E14] border-none text-[14px] font-medium whitespace-nowrap text-white  ml-1 capitalize'>
+                            <FaLock className='w-[13px] h-[13px] mr-[3px]' />
+                            premium
+                          </span>
+                        </span>
+                      </div>
+                      <p className='mb-0 text-[#7b7b8a] text-[16px] font-semibold'>
+                        25m 10s
+                      </p>
+                    </div>
                   </div>
                 </details>
               </div>
