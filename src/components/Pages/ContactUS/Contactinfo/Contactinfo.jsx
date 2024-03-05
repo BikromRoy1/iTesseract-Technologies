80dc98d9ebed7a368c59f29b8078d69f67fa81fe
BikromRoy1 <bikrom@peoplentech.com.bd>
2024-03-05 16:33:03 +0600

    contact us page add section title and color add

--- src/components/Pages/ContactUS/Contactinfo/Contactinfo.jsx
+++ src/components/Pages/ContactUS/Contactinfo/Contactinfo.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
 import axios from 'axios';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import './Contactinfo.css';
+import React, { useEffect, useState } from 'react';
+import { BiEnvelope, BiMapAlt } from 'react-icons/bi';
 import { FaPhoneAlt } from 'react-icons/fa';
-import { BiMapAlt, BiEnvelope } from 'react-icons/bi';
+import './Contactinfo.css';
 
-const Contactinfo = ({ title, details }) => {
-  
+const Contactinfo = ({ title, details, subTitle }) => {
   const [contact, setContact] = useState([]);
 
   useEffect(() => {
@@ -25,49 +22,38 @@ const Contactinfo = ({ title, details }) => {
   }, []);
 
   return (
-    <div className='bg-gray-100'>
-      <div className='text-center pt-12'>
-        <h1 className='text-[#1bb57b] font-semibold mb-4 md:text-4xl text-3xl'>
-          {title}
-        </h1>
-        <p className='text-gray-400'>{details}</p>
-      </div>
-      <section className=''>
-        <div className='container px-6 py-12 mx-auto'>
-          <div className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3'>
-            <div class='info-item  flex flex-col justify-center items-center'>
-              <div className='contact-icons'>
-                <BiMapAlt className='' size='24px' color='#ffc107'></BiMapAlt>
-              </div>
-              <h3>আমাদের ঠিকানা</h3>
-              <p className='text-base font-medium'>{contact?.data?.address}</p>
+    <section className='bg-gray-100 contact-info'>
+      <div className='container p-6  mx-auto'>
+        <div className='section-header'>
+          <h4>{subTitle}</h4>
+          <h2 className='title-gradient'>{title}</h2>
+          <p>{details}</p>
+        </div>
+        <div className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3'>
+          <div class='info-item  flex flex-col justify-center items-center'>
+            <div className='contact-icons'>
+              <BiMapAlt className='' size='24px' color='#ffc107'></BiMapAlt>
             </div>
-            <div class='info-item  flex flex-col justify-center items-center'>
-              <div className='contact-icons'>
-                <FaPhoneAlt
-                  className=''
-                  size='24px'
-                  color='#ffc107'
-                ></FaPhoneAlt>
-              </div>
-              <h3>আমাদের কল করুন</h3>
-              <p className='text-base font-medium'>{contact?.data?.phone}</p>
+            <h3>আমাদের ঠিকানা</h3>
+            <p className='text-base font-medium'>{contact?.data?.address}</p>
+          </div>
+          <div class='info-item  flex flex-col justify-center items-center'>
+            <div className='contact-icons'>
+              <FaPhoneAlt className='' size='24px' color='#ffc107'></FaPhoneAlt>
             </div>
-            <div class='info-item  flex flex-col justify-center items-center'>
-              <div className='contact-icons'>
-                <BiEnvelope
-                  className=''
-                  size='24px'
-                  color='#ffc107'
-                ></BiEnvelope>
-              </div>
-              <h3>আমাদেরকে ইমেইল করুন</h3>
-              <p className='text-base font-medium'>{contact?.data?.email}</p>
+            <h3>আমাদের কল করুন</h3>
+            <p className='text-base font-medium'>{contact?.data?.phone}</p>
+          </div>
+          <div class='info-item  flex flex-col justify-center items-center'>
+            <div className='contact-icons'>
+              <BiEnvelope className='' size='24px' color='#ffc107'></BiEnvelope>
             </div>
+            <h3>আমাদেরকে ইমেইল করুন</h3>
+            <p className='text-base font-medium'>{contact?.data?.email}</p>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
