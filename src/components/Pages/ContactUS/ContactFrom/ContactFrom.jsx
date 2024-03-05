edcb7f11a903980814f52452859540f1a06f2bf8
BikromRoy1 <bikrom@peoplentech.com.bd>
2024-03-05 16:47:13 +0600

    Contact us page contact from

--- src/components/Pages/ContactUS/ContactFrom/ContactFrom.jsx
+++ src/components/Pages/ContactUS/ContactFrom/ContactFrom.jsx
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 import contactAnimation from '../../../../Images/contact.gif';
 import './ContactFrom.css';
 
-const ContactFrom = ({ title }) => {
+const ContactFrom = ({ title, subTitle }) => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     const form = event.target;
@@ -31,121 +31,115 @@ const ContactFrom = ({ title }) => {
   };
 
   return (
-    <div className='bg-white'>
-      <div className='text-center pt-10'>
-        <h1 className='text-[#1bb57b] font-semibold  md:text-4xl text-3xl'>
-          {title}
-        </h1>
-      </div>
-      <section className='bg-white'>
-        <div className='container px-6 py-10 mx-auto lg:py-2'>
-          <div className='lg:flex items-center'>
-            <div className='lg:w-1/2'>
-              <img
-                loading='lazy'
-                className='sm:w-full lg:w-[500px]'
-                src={contactAnimation}
-                alt='contact-animation'
-              />
-            </div>
+    <section className='bg-white contact-from'>
+      <div className='container p-6 mx-auto'>
+        <div className='section-header'>
+          <h4>{ subTitle}</h4>
+          <h2 className='title-gradient'>{title}</h2>
+        </div>
+        <div className='lg:flex items-center'>
+          <div className='lg:w-1/2'>
+            <img
+              loading='lazy'
+              className='sm:w-full lg:w-[500px]'
+              src={contactAnimation}
+              alt='contact-animation'
+            />
+          </div>
 
-            <div className='mt-8 lg:w-1/2 lg:mt-0'>
-              <form
-                onSubmit={handleSubmit}
-                className='container flex flex-col mx-auto space-y-12'
-              >
-                <fieldset className='grid grid-cols-6 gap-6 '>
-                  <div className='grid grid-cols-6 gap-4 col-span-full lg:col-span-12'>
-                    <div className='col-span-full sm:col-span-3'>
-                      <input
-                        id='username'
-                        type='text'
-                        name='username'
-                        placeholder='ব্যবহারকারীর নাম'
-                        className='w-full input-from-contorl p-3 rounded-md bg-white text-black'
-                        required
-                      />
-                    </div>
-                    <div className='col-span-full sm:col-span-3'>
-                      <input
-                        id='email'
-                        type='email'
-                        name='email'
-                        placeholder='ইমেইল'
-                        className='w-full p-3 rounded-md input-from-contorl  text-gray-900'
-                        required
-                      />
-                    </div>
-                    <div className='col-span-full sm:col-span-3'>
-                      <input
-                        id='phone'
-                        type='text'
-                        name='phone'
-                        placeholder='তোমার নাম্বার'
-                        className='w-full p-3 rounded-md input-from-contorl text-gray-900'
-                        required
-                      />
-                    </div>
-                    <div className='col-span-full sm:col-span-3'>
-                      <input
-                        id='subject'
-                        type='text'
-                        name='subject'
-                        placeholder='বিষয়'
-                        className='w-full p-3 rounded-md input-from-contorl text-gray-900'
-                        required
-                      />
-                    </div>
-                    <div className='col-span-full'>
-                      <textarea
-                        id='bio'
-                        cols='10'
-                        rows='4'
-                        name='message'
-                        placeholder='আপনার মেসেজেস'
-                        className='w-full p-3 rounded-md input-from-contorl text-gray-900'
-                        required
-                      ></textarea>
-                    </div>
-                    <div className='col-span-full'>
-                      <div className='flex items-center gap-3'>
-                        <span>
-                          <input
-                            type='checkbox'
-                            name='teramAndCondition'
-                            id=''
-                            required
-                          />
-                        </span>
-                        <p className='text-gray-400'>
-                          I agree to the terms of data processing.{' '}
-                          <a
-                            className='text-[#1bb57b] font-medium'
-                            href='/terms'
-                          >
-                            Terms and Conditions
-                          </a>
-                        </p>
-                      </div>
-                    </div>
-                    <div className='col-span-full'>
-                      <div className='text-center'>
-                        <button
-                          type='submit'
-                          className='w-full px-8 py-3 font-semibold rounded-md bg-[#1bb57b] text-white'
-                        >
-                          Send A Message
-                        </button>
-                      </div>
+          <div className='mt-8 lg:w-1/2 lg:mt-0'>
+            <form
+              onSubmit={handleSubmit}
+              className='container flex flex-col mx-auto space-y-12'
+            >
+              <fieldset className='grid grid-cols-6 gap-6 '>
+                <div className='grid grid-cols-6 gap-4 col-span-full lg:col-span-12'>
+                  <div className='col-span-full sm:col-span-3'>
+                    <input
+                      id='username'
+                      type='text'
+                      name='username'
+                      placeholder='ব্যবহারকারীর নাম'
+                      className='w-full input-from-contorl p-3 rounded-md bg-white text-black'
+                      required
+                    />
+                  </div>
+                  <div className='col-span-full sm:col-span-3'>
+                    <input
+                      id='email'
+                      type='email'
+                      name='email'
+                      placeholder='ইমেইল'
+                      className='w-full p-3 rounded-md input-from-contorl  text-gray-900'
+                      required
+                    />
+                  </div>
+                  <div className='col-span-full sm:col-span-3'>
+                    <input
+                      id='phone'
+                      type='text'
+                      name='phone'
+                      placeholder='তোমার নাম্বার'
+                      className='w-full p-3 rounded-md input-from-contorl text-gray-900'
+                      required
+                    />
+                  </div>
+                  <div className='col-span-full sm:col-span-3'>
+                    <input
+                      id='subject'
+                      type='text'
+                      name='subject'
+                      placeholder='বিষয়'
+                      className='w-full p-3 rounded-md input-from-contorl text-gray-900'
+                      required
+                    />
+                  </div>
+                  <div className='col-span-full'>
+                    <textarea
+                      id='bio'
+                      cols='10'
+                      rows='4'
+                      name='message'
+                      placeholder='আপনার মেসেজেস'
+                      className='w-full p-3 rounded-md input-from-contorl text-gray-900'
+                      required
+                    ></textarea>
+                  </div>
+                  <div className='col-span-full'>
+                    <div className='flex items-center gap-3'>
+                      <span>
+                        <input
+                          type='checkbox'
+                          name='teramAndCondition'
+                          id=''
+                          required
+                        />
+                      </span>
+                      <p className='text-gray-400'>
+                        I agree to the terms of data processing.{' '}
+                        <a className='text-[#1bb57b] font-medium' href='/terms'>
+                          Terms and Conditions
+                        </a>
+                      </p>
                     </div>
                   </div>
-                </fieldset>
-              </form>
-            </div>
+                  <div className='col-span-full'>
+                    <div className='text-center'>
+                      <button
+                        type='submit'
+                        className='w-full px-8 py-3 font-semibold rounded-md bg-[#1bb57b] text-white'
+                      >
+                        Send A Message
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </fieldset>
+            </form>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
--- src/components/Pages/ContactUS/ContactUS/ContactUS.jsx
+++ src/components/Pages/ContactUS/ContactUS/ContactUS.jsx
@@ -19,7 +19,10 @@ const ContactUS = () => {
         details='যে কোনো প্রয়োজনে যোগাযোগ করতে সরাসরি আমাদের অফিস ভিজিট করতে পারেন। তাছাড়া নম্বরে কল করে জেনে নিতে পারেন যেকোনো তথ্য। এছাড়াও উল্লেখিত মেইলে কিংবা ফেসবুক ম্যাসেঞ্জারেও নক দিতে পারেন। '
         subTitle='যোগাযোগ করুন'
       ></Contactinfo>
-      <ContactFrom title='আমাদের বিশেষজ্ঞরা আপনাকে সাহায্য করবে'></ContactFrom>
+      <ContactFrom
+        title='আমাদের বিশেষজ্ঞরা আপনাকে সাহায্য করবে'
+        subTitle='আপনাদের জিজ্ঞাসা ইনবক্স করুন'
+      ></ContactFrom>
       <GoogleMap mapLink='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.9074736547177!2d90.3846032143484!3d23.750678694684026!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b908bb495dd7%3A0x86ae1964522bfd37!2siZoom%20Limited!5e0!3m2!1sen!2sbd!4v1674923467425!5m2!1sen!2sbd'></GoogleMap>
     </div>
   );
