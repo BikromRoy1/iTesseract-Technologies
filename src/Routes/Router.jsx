29432db7c6f96194dd63112dff7d2030bb4a15f4
BikromRoy1 <bikrom@peoplentech.com.bd>
2024-03-09 11:40:10 +0600

    Create a login page

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
+import Main from '../Layout/Main';
 import ErrorPage from '../components/Common/ErrorPage/ErrorPage';
 import Policy from '../components/Common/Policy/Policy';
 import Registration from '../components/Common/Registration/Registration';
@@ -8,15 +9,14 @@ import BlogDetails from '../components/Pages/BlogDetails/BlogDetails/BlogDetails
 import Blogs from '../components/Pages/Blogs/Blogs/Blogs';
 import Certification from '../components/Pages/Certification/Certification';
 import ContactUS from '../components/Pages/ContactUS/ContactUS/ContactUS';
+import CourseDetails from '../components/Pages/CourseDetails/CourseDetails';
+import VideosPlaying from '../components/Pages/CourseDetails/videosPlaying/VideosPlaying.jsx';
 import Home from '../components/Pages/Home/Home/Home';
 import ProductDetails from '../components/Pages/Product/ProductDetails/ProductDetails';
 import ProductMain from '../components/Pages/Product/ProductMain/ProductMain';
 import School from '../components/Pages/School/School/School';
 import Team from '../components/Pages/Team/Team/Team';
 import University from '../components/Pages/University/University/University';
-import Main from '../Layout/Main';
-import CourseDetails from '../components/Pages/CourseDetails/CourseDetails';
-import VideosPlaying from '../components/Pages/CourseDetails/videosPlaying/VideosPlaying.jsx';
 
 const router = createBrowserRouter([
   {
