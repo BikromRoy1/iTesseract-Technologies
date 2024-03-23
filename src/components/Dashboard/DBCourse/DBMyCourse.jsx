217866680a4fb4b4530e946a12ad713c80b33c47
BikromRoy1 <bikrom@peoplentech.com.bd>
2024-03-23 13:03:51 +0600

    my course enroll course add

--- src/components/Dashboard/DBCourse/DBMyCourse.jsx
+++ src/components/Dashboard/DBCourse/DBMyCourse.jsx
@@ -1,7 +1,54 @@
 import React from 'react';
+import course01 from '../../../Images/course-01.jpg';
+import course02 from '../../../Images/course-02.jpg';
 
 const DBMyCourse = () => {
-  return <div>DBCourse</div>;
+  const courses = [
+    {
+      course_id: '01',
+      course: 'কম্পিউটার প্রোগ্রামিং ফান্ডামেন্টাল',
+      courseBanner: course01,
+    },
+    {
+      course_id: '02',
+      course: 'মোবাইল অ্যাপ্লিকেশন ও ওয়েবসাইট ডেভেলপমেন্ট',
+      courseBanner: course02,
+    },
+  ];
+
+  return (
+    <section className='dashboard-section'>
+      <div className='dashboard-box'>
+        <div className='box-header flex justify-between items-center'>
+          <div className='box-title relative '>আমার কোর্সসমূহ</div>
+        </div>
+        <div className='p-[1.25rem]'>
+          <div className='grid grid-cols-1 gap-x-4 lg:grid-cols-3 lg:gap-x-6'>
+            {courses.map((course) => (
+              <div key={course.course_id} className='rounded-lg bg-[#f5f5f5]'>
+                <div>
+                  <img
+                    className='rounded-t-lg'
+                    src={course.courseBanner}
+                    loading='lazy'
+                    alt={course.course}
+                  />
+                </div>
+                <div className='p-3'>
+                  <h2 className='font-semibold mb-3 text-[16px] md:text-[18px] lg:text-[20px]'>
+                    {course.course}
+                  </h2>
+                  <button className='btn-buy mb-0 rounded-md bg-[#1CAB55] p-3 whitespace-nowrap text-base font-semibold text-white md:w-full  '>
+                    এগিয়ে যাই
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default DBMyCourse;
