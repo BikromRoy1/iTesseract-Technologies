7a10c1fd1c97e5ad3ba0ae610dd734bb1df2ba1e
BikromRoy1 <bikrom@peoplentech.com.bd>
2024-03-24 12:57:52 +0600

    Assignment page add real time chart . it is dynamic chart

--- src/components/Dashboard/Analytics/Analytics.jsx
+++ src/components/Dashboard/Analytics/Analytics.jsx
@@ -8,18 +8,20 @@ const Analytics = () => {
       <div className=''>
         <div className='grid grid-cols-1 gap-4 md:grid-cols-1 lg:grid-cols-3 lg:gap-6'>
           <div className='dashboard-box min-h-[250px]'>
-            <div class='box-header'>
-              <div class='box-title relative '>কোর্স স্কোর</div>
+            <div className='box-header'>
+              <div className='box-title relative '>কোর্স স্কোর</div>
             </div>
             <div className='p-[1.25rem] pt-2'>
               <Score />
             </div>
           </div>
           <div className='min-h-[250px] dashboard-box lg:col-span-2'>
-            <div class='box-header'>
-              <div class='box-title relative '>অ্যাসাইনমেন্ট অ্যানালিটিক্স</div>
+            <div className='box-header'>
+              <div className='box-title relative '>
+                অ্যাসাইনমেন্ট অ্যানালিটিক্স
+              </div>
             </div>
-            <div className='p-[1.25rem]'>
+            <div className='p-[1.25rem] pt-2'>
               <Assignment />
             </div>
           </div>
