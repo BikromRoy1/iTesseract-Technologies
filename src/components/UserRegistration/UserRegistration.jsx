589d4ebd6dc05894c56dae46ba96682c606c6988
BikromRoy1 <bikrom@peoplentech.com.bd>
2024-05-02 18:52:56 +0600

    password input filed add showpassword button

--- src/components/UserRegistration/UserRegistration.jsx
+++ src/components/UserRegistration/UserRegistration.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import roundedLogo from '../../Images/Round-logo.png';
 import mainLogo from '../../Images/main-logo.svg';
 import './UserRegistration.css';
 
 const UserRegistration = () => {
-    
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     const form = event.target;
@@ -109,24 +114,70 @@ const UserRegistration = () => {
             </div>
 
             <div className='mt-4'>
-              <div className='flex justify-between'>
-                <label
-                  className='block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200'
-                  htmlFor='loggingPassword'
-                >
-                  Password
-                </label>
+              <div className='flex flex-col my-4'>
+                <div className='flex justify-between'>
+                  <label
+                    className='block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200'
+                    htmlFor='loggingPassword'
+                  >
+                    Password
+                  </label>
+                </div>
+                <div className='relative flex items-center'>
+                  <input
+                    type={showPassword ? 'text' : 'password'}
+                    id='loggingPassword'
+                    name='password'
+                    required
+                    className='flex-1 px-4 py-2  border-gray-300 block w-full text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300'
+                    placeholder='পাসওয়ার্ড'
+                    autoComplete='on'
+                  />
+                  <button
+                    onClick={togglePasswordVisibility}
+                    type='button'
+                    className='absolute right-2 bg-transparent flex items-center justify-center text-gray-700'
+                  >
+                    {!showPassword ? (
+                      <svg
+                        className='w-5 h-5'
+                        fill='none'
+                        stroke='currentColor'
+                        viewBox='0 0 24 24'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <path
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
+                          strokeWidth='2'
+                          d='M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21'
+                        ></path>
+                      </svg>
+                    ) : (
+                      <svg
+                        className='w-5 h-5'
+                        fill='none'
+                        stroke='currentColor'
+                        viewBox='0 0 24 24'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <path
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
+                          strokeWidth='2'
+                          d='M15 12a3 3 0 11-6 0 3 3 0 016 0z'
+                        ></path>
+                        <path
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
+                          strokeWidth='2'
+                          d='M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z'
+                        ></path>
+                      </svg>
+                    )}
+                  </button>
+                </div>
               </div>
-
-              <input
-                placeholder='পাসওয়ার্ড'
-                id='loggingPassword'
-                name='password'
-                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300'
-                type='password'
-                required
-                autoComplete='on'
-              />
             </div>
 
             <div className='mt-6'>
