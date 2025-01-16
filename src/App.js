c234e8aa21e707839a1254f104d26036cecdc885
BikromRoy1 <bikrom@peoplentech.com.bd>
2025-01-16 14:29:04 +0600

    whatup button add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { RouterProvider } from "react-router-dom";
-import "./App.css";
-import router from "./Routes/Router";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { RouterProvider } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
+import router from './Routes/Router';
+import WhatsAppButton from './components/WhatsAppButton/WhatsAppButton';
 
 function App() {
   return (
     <div>
-      <ToastContainer position="top-center" />
+      <ToastContainer position='top-center' />
       <RouterProvider router={router}></RouterProvider>
+      <WhatsAppButton />
     </div>
   );
 }
